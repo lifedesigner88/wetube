01250793e16a4e4f4de071470307cf886feebfc1
sejong_LepTop <lifedesigner88@gmail.com>
2023-10-27 23:28:50 +0900

    nodeUP mongoDB for Session

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,8 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube!!";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.username = req.session.user.username;
+  if(res.locals.loggedIn==true){
+  res.locals.username = req.session.user.username;}
   console.log(res.locals);
   next();
 };
